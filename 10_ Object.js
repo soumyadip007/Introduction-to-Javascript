754f58e9b8eb31f09c2655493c8b477f756ace69
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-11-17 21:00:48 +0530

    Object in JS

---
+++ 10_ Object.js
@@ -0,0 +1,11 @@
+var user={
+    name: "Soumyadip",
+    role: "Dev",
+    loginCount: 99
+}
+
+console.log(user)
+console.log(user.name)
+console.log(user.role)
+console.log(user.loginCount)
+console.log(user["name"])
