0aef4afb0fb10153d301b8ecf02850e769877d06
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-11-17 21:43:52 +0530

    Proto

--- 13_ Proto in JS.js
+++ 13_ Proto in JS.js
@@ -4,16 +4,20 @@ var user= function(name, course){
     this.getCourse=function(){
         console.log(name)
         console.log(course)
-    },
+    };
 
     this.getCourse1 = () =>{
         console.log(name)
         console.log(course)
-
-    }
+    };
+};
+user.def.yoyo = function(){
+    console.log(this.name);
 }
 
 
 var soumya= new user("Soumya","Java")
 soumya.getCourse()
 soumya.getCourse1()
+
+
