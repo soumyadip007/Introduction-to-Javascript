c2adce751f899429611d706d7621497aecc4669e
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-11-17 20:57:06 +0530

    callBack and Arrow Function

---
+++ 9_ Callack and Arrow.js
@@ -0,0 +1,18 @@
+//Arrow Function
+var isEven= (hey) =>{
+  
+    return hey%2===0
+};
+
+console.log(isEven(2))
+
+
+var test=[4,4,4,4,4].every(isEven)
+console.log(test)
+
+
+var test=[4,4,4,4,4].every((e)=>{
+    console.log(e)
+    return e%2===0
+})
+console.log(test)
