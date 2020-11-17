772346c52029027982194382f3505d910317e0ec
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-11-17 20:31:21 +0530

    Function

---
+++ 6_ Function.js
@@ -0,0 +1,18 @@
+function sayHello(name){
+
+    console.log("Hey, ",name)
+    console.log("Hey, ",name,"aasdad")
+}
+
+sayHello('soumyadip')
+sayHello(1121)
+
+function yo(name){
+
+    console.log("Hey, ",name)
+    console.log("Hey, ",name,"aasdad")
+
+    return name+" Yo"
+}
+let p=yo('soumyadip')
+console.log(p)
