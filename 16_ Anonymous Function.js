ced2353ca3b7f782acb94f9b8a4d0a2fcfef9009
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-11-18 12:42:14 +0530

    Hidden Func

---
+++ 16_ Anonymous Function.js
@@ -0,0 +1,14 @@
+(function(){
+    console.log("I Say Hello");
+    console.log("I again say Hello")
+})();
+
+var sayHello=() =>{(function(){
+    console.log("I Say Hello");
+    console.log("I again say Hello")
+})();
+    console.log("I Say Hello");
+    console.log("I again say Hello")
+}
+
+sayHello()
