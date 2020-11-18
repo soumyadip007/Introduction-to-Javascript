de122280eb1efbd6078ee3f0c7d59e3cc72df300
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-11-18 17:58:00 +0530

    Event Queue

---
+++ 22_ Event Loop.js
@@ -0,0 +1,24 @@
+const uno = () =>{
+    console.log("Hi, I'm a uno");
+}
+
+const dos = () =>{
+    setTimeout(()=>{
+        console.log("Hello, Timer")
+    },3000)
+    console.log("Hi, I'm a dos");
+}
+
+const tres = () =>{
+    console.log("Hi, I'm a tres");
+}
+
+
+// uno()
+// tres()
+// dos()
+
+
+uno()
+dos()
+tres()
