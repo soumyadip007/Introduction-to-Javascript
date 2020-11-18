cb2218ec5a7057a5f040356b9362ac7c6b195d45
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-11-18 18:13:02 +0530

    Promise Async Await Resolve

--- 23_ Promise Async Await Resolve.js
+++ 23_ Promise Async Await Resolve.js
@@ -2,11 +2,22 @@ const uno = () =>{
     return "Hi, I'm a uno";
 }
 
-const dos = async () =>{
+const dos1 = async () =>{
     setTimeout(()=>{
         return "Hello, Timer";
     },3000);
-   // return "Hi, I'm a dos";
+ //  return "Hi, I'm a dos";
+}
+
+const dos = async () =>{
+    return new Promise((resolve,reject)=>{
+        // resolve("I got that");
+        setTimeout(()=>{
+            resolve("I got that")
+           // reject("I got that")
+            //  return "Hi, I'm a dos";
+    },3000);
+    })
 }
 
 const tres = () =>{
@@ -18,11 +29,14 @@ const tres = () =>{
 // tres()
 // dos()
 
-const call = () =>{
+const call = async() =>{
     let unoVal= uno();
     console.log(unoVal);
 
-    let dosVal= dos();
+    let dosVal1= await dos();
+    console.log(dosVal1);
+
+    let dosVal= await dos();
     console.log(dosVal);
 
     let tresVal= tres();
