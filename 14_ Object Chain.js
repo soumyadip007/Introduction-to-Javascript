a81fc75179e9579f01ac4737863b52edeed9dfa3
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-11-18 12:41:49 +0530

    Object Chain

---
+++ 14_ Object Chain.js
@@ -0,0 +1,26 @@
+var user= function(name, course){
+    this.name=name;
+    this.course=course;
+    this.getCourse=function(){
+        console.log(name)
+        console.log(course)
+    };
+
+    this.getCourse1 = () =>{
+        console.log(name)
+        console.log(course)
+    };
+};
+// user.def.yoyo = function(){
+//     console.log(this.name);
+// }
+
+
+var soumya= new user("Soumya","Java")
+soumya.getCourse()
+soumya.getCourse1()
+
+//soumya.yoyo
+
+if(soumya.hasOwnProperty("name"))
+    console.log("Hey")
